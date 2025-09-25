77a24331fc907038786c19064a6597f572abba92
Philip Karlsson <karlssonphilip1992@gmail.com>
2025-09-25 13:04:15 +0200

    Lägg till basmeny med switch statement

--- src/Kalkylator.java
+++ src/Kalkylator.java
@@ -1,5 +1,55 @@
+import java.awt.*;
+import java.util.Scanner;
+
 public class Kalkylator {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Välkommmen till Kalkylatorn");
+
+        while(true){
+
+            System.out.println("Vad vill du räkna idag?" + " " + "Välj ett alternativ");
+            System.out.println("1. Addition +");
+            System.out.println("2. Subtrahera -");
+            System.out.println("3. Multiplikation *");
+            System.out.println("4. Division /");
+            System.out.println("0. Avsluta");
+
+            int val =  input.nextInt();
+            switch(val)
+            {
+                case 1:
+                    System.out.println("Du valde 1. Addition +");
+                    break;
+                case 2:
+                    System.out.println("Du valde 2. Subtrahera -");
+                    break;
+                case 3:
+                    System.out.println("Du valde 3. Multiplikation *");
+                    break;
+                case 4:
+                    System.out.println("Du valde 4. Division /");
+                    break;
+                case 0:
+                    System.out.println("Du har valt att avsluta programmet! Ha en bra dag! ");
+                    return;
+                default:
+                    System.out.println("Oj du måste tryckt på fel knapp testa igen med en siffra mellan 4 till 0");
+                    break;
+
+
+
+
+            }
+
+
+
+
+        }
+
+
+
 
     }
 }
